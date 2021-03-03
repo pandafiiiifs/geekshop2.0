64d35a648e4713b9e8f4a66698cf7ae875c269cc
pandafiiiifs <66410903+pandafiiiifs@users.noreply.github.com>
2021-03-03 13:23:58 +0300

    media from bd

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -14,6 +14,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 from mainapp.views import index, products
 
@@ -24,3 +27,6 @@ urlpatterns = [
 
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- mainapp/views.py
+++ mainapp/views.py
@@ -4,6 +4,7 @@ import json
 dir = os.path.dirname(__file__)
 
 from django.shortcuts import render
+from mainapp.models import Poduct, ProductCategory
 
 # Create your views here.
 def index(request):
@@ -13,26 +14,8 @@ def index(request):
 def products(request):
     context = {
         'tittle': "GeekShop",
-        'products': [
-            {'name': 'Худи черного цвета с монограммами adidas Originals', 'price': '6 090,00 руб.',
-             'description': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.',
-             'img': 'vendor/img/products/Adidas-hoodie.png'},
-            {'name': 'Синяя куртка The North Face', 'price': '23 725,00 руб. руб.',
-             'description': 'Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель.',
-             'img': 'vendor/img/products/Blue-jacket-The-North-Face.png'},
-            {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN', 'price': '3 390,00 руб.',
-             'description': 'Материал с плюшевой текстурой. Удобный и мягкий.',
-             'img': 'vendor/img/products/Brown-sports-oversized-top-ASOS-DESIGN.png'},
-            {'name': 'Черный рюкзак Nike Heritage', 'price': '2 340,00 руб.',
-             'description': 'Плотная ткань. Легкий материал.',
-             'img': 'vendor/img/products/Black-Nike-Heritage-backpack.png'},
-            {'name': 'Черные туфли на платформе с 3 парами люверсов Dr Martens 1461 Bex', 'price': '13 590,00 руб.',
-             'description': 'Гладкий кожаный верх. Натуральный материал.',
-             'img': 'vendor/img/products/Black-Dr-Martens-shoes.png'},
-            {'name': 'Темно-синие широкие строгие брюки ASOS DESIGN', 'price': '2 890,00 руб.',
-             'description': 'Легкая эластичная ткань сирсакер Фактурная ткань.',
-             'img': 'vendor/img/products/Dark-blue-wide-leg-ASOs-DESIGN-trousers.png'},
-        ]
+        'products': Poduct.objects.all(),
+        'categories': Poduct.objects.all(),
     }
     file_path = os.path.join(dir,'fixtures/products.json')
     context.update(json.load(open(file_path,encoding='utf-8')))
