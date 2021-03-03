1c967851b504c6debc2c0b3913c9c46e07249945
pandafiiiifs <66410903+pandafiiiifs@users.noreply.github.com>
2021-03-03 13:38:52 +0300

    подключил url

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,24 +1,20 @@
-import os
-import json
-
-dir = os.path.dirname(__file__)
-
 from django.shortcuts import render
+
 from mainapp.models import Poduct, ProductCategory
 
-# Create your views here.
-def index(request):
-    context = {'title': 'GeekShop'}
-    return render(request, 'mainapp/index.html', context)
 
-def products(request):
-    context = {
-        'tittle': "GeekShop",
-        'products': Poduct.objects.all(),
-        'categories': Poduct.objects.all(),
+def main(request):
+    content = {
+        'title': 'GeekShop'
     }
-    file_path = os.path.join(dir,'fixtures/products.json')
-    context.update(json.load(open(file_path,encoding='utf-8')))
-    return render(request, 'mainapp/products.html', context)
+    return render(request, 'mainapp/index.html', content)
 
+
+def products(request, id=None):
+    content = {
+        'title': 'GeekShop - Категории',
+        'categories': ProductCategory.objects.all(),
+        'products': Poduct.objects.all(),
+    }
+    return render(request, 'mainapp/products.html', content)
 
