9892d302ba1da1232f2ebc703c6d16d98d18b3c4
pandafiiiifs <pandafiiiifs@gmail.com>
2021-03-22 13:04:26 +0300

    paginator fix

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from mainapp.models import Product, ProductCategory
 
@@ -13,7 +13,18 @@ def index(request):
 def products(request, category_id=None, page=1):
     context = {'title': 'GeekShop - Каталог', 'categories': ProductCategory.objects.all()}
     if category_id:
-        context.update({'products': Product.objects.filter(category_id= category_id)})
+        products = Product.objects.filter(category_id=category_id)
     else:
-        context.update({'products': Product.objects.all()})
+        products = Product.objects.all()
+    paginator = Paginator(products, 3)
+    try:
+        products_paginator = paginator.page(page)
+    except PageNotAnInteger:
+        products_paginator = paginator.page(1)
+    except EmptyPage:
+        products_paginator = paginator.page(paginator.num_pages)
+
+    context.update({'products': products_paginator })
+
+
     return render(request, 'mainapp/products.html', context)
