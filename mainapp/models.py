1bde0a9229f3f5c906fd654a64dc998194596bb5
pandafiiiifs <66410903+pandafiiiifs@users.noreply.github.com>
2021-04-15 22:47:24 +0300

    fix mainapp

--- mainapp/models.py
+++ mainapp/models.py
@@ -15,7 +15,7 @@ class ProductCategory(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=256)
     description = models.TextField(blank=True)
-    short_description = models.CharField(max_length=255, blank=True)
+    short_description = models.CharField(max_length=500, blank=True)
     price = models.DecimalField(max_digits=8, decimal_places=2, default=0)
     quantity = models.PositiveIntegerField(default=0)
     image = models.ImageField(upload_to='products_images', blank=True)
