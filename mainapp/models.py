e8f1d723fa67aa1be75dc6145953512973a32b87
pandafiiiifs <66410903+pandafiiiifs@users.noreply.github.com>
2021-04-15 22:35:16 +0300

    fix mainapp

--- mainapp/models.py
+++ mainapp/models.py
@@ -15,12 +15,19 @@ class ProductCategory(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=256)
     description = models.TextField(blank=True)
-    short_description = models.CharField(max_length=255, blank=True)
+    short_description = models.CharField(max_length=64, blank=True)
     price = models.DecimalField(max_digits=8, decimal_places=2, default=0)
     quantity = models.PositiveIntegerField(default=0)
     image = models.ImageField(upload_to='products_images', blank=True)
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    is_active = models.BooleanField(
+        verbose_name='активен',
+        default=True
+    )
 
     def __str__(self):
         return f'{self.name} | {self.category.name}'
 
+    @staticmethod
+    def get_items():
+        return Product.objects.filter(is_active=True).order_by('category', 'name')
